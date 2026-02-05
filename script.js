76ea3e190658916f216c683d698b7e3d3f9a22a5
Caitlin Walker <caitybeanie@Caitlins-MacBook-Air.local>
2026-02-05 13:12:31 +0100

    Initial comit 💌

---
+++ script.js
@@ -0,0 +1,54 @@
+//elements
+
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+//envelope click
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout(() => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+});
+
+// grow yes button
+let yesScale = 1;
+yesBtn.style.position = "relative";
+yesBtn.style.transformOrigin ="center center";
+yesBtn.style.transition = "transform 0.3s ease";
+
+//no button
+noBtn.addEventListener("click", () =>{
+    yesScale +=2;
+
+    if(yesBtn.style.position !== "fixed"){
+        yesBtn.style.position = "fixed";
+        yesBtn.style.top = "50%";
+        yesBtn.style.left = "50%";
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }else{
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }
+});
+
+// Yes click
+yesBtn.addEventListener("click",  () =>{
+    title.textContent = "Yayyyyyyy!✨";
+
+    catImg.src = "images/cat_dance.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+});
